be6873d03c4b77acd16d4c6b03d3b38548a10488
Suraj Kumar <znsuraj7@gmail.com>
2022-08-19 16:09:47 +0530

    added

---
+++ _next/static/Ye4dFHC5jExLzFxgsPakO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-471940978c8c0ba0.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/about":["static/chunks/pages/about-4f1a4b79d43775c6.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
